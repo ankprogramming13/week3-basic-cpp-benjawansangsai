821a17d4ba75d669e6dadb6235b8ea4823b2d7b7
Unknown <benjawan.0052232@gmail.com>
2017-11-21 17:25:48 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,9 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Miss." << endl << "Benjawan Sanjsai" << endl << "Bee" << endl << "Female" << endl << "13 April 1999" << endl << "Play Game" << endl << "Divorce";
+
+
   return 0;
 }
